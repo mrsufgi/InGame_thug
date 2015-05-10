ef4b780f086042ca8669d028e10b2a9fa2f074c9
michrabi <michalrabinowitch@michals-MacBook-Pro-3.local>
2015-05-10 16:37:22 +0300

    Map Manager

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+<<<<<<< HEAD
 
 public class MapManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
+
+using UnityEngine.UI;
+
+public class MapManager : MonoBehaviour {
+
+    public Button[] buttons;
+	// Use this for initialization
+	void Start () {
+	    for( int i = 0; i < buttons.Length; i++)
+        {
+            //Lock all Buttona except 1
+            if (i != 1)
+                buttons[i].interactable = false;
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 }
