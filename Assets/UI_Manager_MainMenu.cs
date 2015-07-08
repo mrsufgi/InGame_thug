4b3190831298f471adbf85bc4a7ef6489814d1be
mrsufgi <sufgis@gmail.com>
2015-07-08 15:11:34 +0300

    anim to menus - commit :?

--- Assets/UI_Manager_MainMenu.cs
+++ Assets/UI_Manager_MainMenu.cs
@@ -4,81 +4,86 @@ using System.Collections;
 
 public class UI_Manager_MainMenu : MonoBehaviour {
 
-	public GameObject settingCanvas;
+    public static GameObject settingCanvas;
     public GameObject shopCanvas;
     public GameObject unlockCanvas;
     public Text soundMode;
-	private static AudioSource settingsClip;
-	public static bool isTutorialOpen =  false;
+    private static AudioSource settingsClip;
+    public static bool isTutorialOpen = false;
 
-	// Use this for initialization
-	void Start () 
-	{
-		settingsClip = gameObject.GetComponent<AudioSource> ();
+    // Use this for initialization
+    void Awake()
+    {
+        settingCanvas = GameObject.FindGameObjectWithTag("Settings");
+        settingsClip = gameObject.GetComponent<AudioSource>();
 
-		if (!isTutorialOpen) {
-			settingCanvas.SetActive(false);
-			settingsClip.Stop ();
-		} else {
-		}
-		settingsClip.Play ();
-	}
+        if (!isTutorialOpen) {
+            settingCanvas.gameObject.GetComponent<Canvas>().enabled = false;
+            settingsClip.Stop();
+        } else {
+            settingCanvas.gameObject.GetComponent<Canvas>().enabled = false;
+        }
+        settingsClip.Play();
+    }
 
 
 
-	public void pausedTime()
-	{
-		Time.timeScale = 0;
-	}
+    public void pausedTime()
+    {
+        Time.timeScale = 0;
+    }
 
-	public void resumeTimePause()
-	{
-		Time.timeScale = 1;
+    public void resumeTimePause()
+    {
+        Time.timeScale = 1;
         settingCanvas.SetActive(false);
-	}
+    }
 
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	public void toggleSound()
-	{
-		if (AudioListener.volume > 0) 
-		{// sound off
-			AudioListener.volume = 0;
-			soundMode.text = "SOUND ON";
-			soundMode.color = Color.yellow;
+    // Update is called once per frame
+    void Update() {
 
-		} 
-		else 
-		{ //sound on
-			AudioListener.volume = 1;
-			soundMode.text = "SOUND OFF";
-			soundMode.color = Color.white;
-		}
-	}
+    }
 
-	public void openSettings()
-	{
-        settingCanvas.gameObject.GetComponent<Canvas>().enabled = true ;
-		if (settingsClip != null) {
-			settingsClip.Play ();
-		}
-	}
+    public void toggleSound()
+    {
+        if (AudioListener.volume > 0)
+        {// sound off
+            AudioListener.volume = 0;
+            soundMode.text = "SOUND ON";
+            soundMode.color = Color.yellow;
 
-	public void closeSettings()
-	{
+        }
+        else
+        { //sound on
+            AudioListener.volume = 1;
+            soundMode.text = "SOUND OFF";
+            soundMode.color = Color.white;
+        }
+    }
+
+    public void openSettings(Animator anim)
+    {
+        anim.SetBool("isOpen", true);
+            settingCanvas.gameObject.GetComponent<Canvas>().enabled = true;
+            if (settingsClip != null)
+            {
+                settingsClip.Play();
+            }
+        }
+
+
+
+    public void closeSettings(Animator anim)
+    {
+
         Debug.Log("close load");
-        settingCanvas.gameObject.GetComponent<Canvas>().enabled = false;
-		if (settingsClip != null) {
-			settingsClip.Stop ();
-		}
-	}
+        anim.SetBool("isOpen", false);      
+    }
 
 
-    public void openShop()
+
+public void openShop()
     {
         shopCanvas.gameObject.SetActive(true);
         //shopCanvas.gameObject.GetComponent<Canvas>().enabled = true;
